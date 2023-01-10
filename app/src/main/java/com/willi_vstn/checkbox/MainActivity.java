49d1fb7e1039054ef89913c0c492c24690472710
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-01-10 12:25:46 -0800

    Inserted Checkbox Inserted RadioGroup Inserted Spinner

--- app/src/main/java/com/willi_vstn/checkbox/MainActivity.java
+++ app/src/main/java/com/willi_vstn/checkbox/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     CheckBox box1, box2;
     Button btn;
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,39 @@ public class MainActivity extends AppCompatActivity {
         box1 = findViewById(R.id.checkbox1);
         box2 = findViewById(R.id.checkbox2);
         btn = findViewById(R.id.btn);
+
+
+
+        RadioGroup radioGroup;
+        radioGroup = findViewById(R.id.radioGroup1);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checked) {
+                RadioButton radioButton = findViewById(checked);
+                Toast.makeText(MainActivity.this, "Selected: "+radioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        spinner = findViewById(R.id.spinner1);
+        String[] courses = {"C++", "Java", "Kotlin", "C#"};
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, courses);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(ad);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Selected: "+courses[i], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +79,10 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }
+
+
         });
+
+
     }
 }
